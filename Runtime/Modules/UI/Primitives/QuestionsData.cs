798c1e6011f55c8975dc1ee1ef953ffded48f5ee
Anthony Shinomiya <anthony@riolab.tv>
2023-02-06 17:24:48 -0300

    add question visualization type

--- Runtime/Modules/UI/Primitives/QuestionsData.cs
+++ Runtime/Modules/UI/Primitives/QuestionsData.cs
@@ -31,11 +31,20 @@ namespace Intelmatix.Data
             [SerializeField] private string title;
             [SerializeField] private string path;
             [SerializeField] private string mode;
+            [SerializeField] private string visualization_type;
 
             public string Title => title;
             public string Path => path;
             public string Mode => mode;
 
+            public enum VisualizationType
+            {
+                None,
+                Points,
+                Arcs,
+                Mumaris,
+            }
+            public VisualizationType Visualization => (VisualizationType)Enum.Parse(typeof(VisualizationType), visualization_type, true);
             public bool IsCognitive => mode.ToLower() == "cognitive";
             public bool IsHumanMode => mode.ToLower().Contains("human");
 
