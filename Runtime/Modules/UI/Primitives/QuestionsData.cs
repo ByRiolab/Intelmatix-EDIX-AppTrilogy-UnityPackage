f08eab148da503fd3fae817e39939cfd69912217
PabloRiolab <122402572+PabloRiolab@users.noreply.github.com>
2023-03-17 18:23:45 -0300

    decide hefx fixed

--- Runtime/Modules/UI/Primitives/QuestionsData.cs
+++ Runtime/Modules/UI/Primitives/QuestionsData.cs
@@ -43,7 +43,7 @@ namespace Intelmatix.Data
                 Points,
                 Arcs,
                 Mumaris,
-                Decide,
+                decide,
             }
             public VisualizationType Visualization => (VisualizationType)Enum.Parse(typeof(VisualizationType), visualization_type, true);
             public bool IsCognitive => mode.ToLower() == "cognitive";
