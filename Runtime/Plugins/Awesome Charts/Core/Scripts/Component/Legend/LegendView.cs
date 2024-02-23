6504000fc035e6fcd80727598a3ad6c995c50302
Jaime Ignacio Arancibia Soto <150153316+JaimeArancibiaDev@users.noreply.github.com>
2024-02-23 18:29:05 -0300

    LegendView

--- Runtime/Plugins/Awesome Charts/Core/Scripts/Component/Legend/LegendView.cs
+++ Runtime/Plugins/Awesome Charts/Core/Scripts/Component/Legend/LegendView.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-namespace AwesomeCharts {
+namespace AwesomeCharts
+{
 
     [ExecuteInEditMode]
     [RequireComponent(typeof(RectTransform))]
-    public class LegendView : ACMonoBehaviour {
+    public class LegendView : ACMonoBehaviour
+    {
 
-        public enum LEGEND_ALIGNMENT {
+        public enum LEGEND_ALIGNMENT
+        {
             TOP_LEFT,
             TOP_RIGHT,
             BOTTOM_LEFT,
             BOTTOM_RIGHT
         }
 
-        public enum LEGEND_ORIENTATION {
+        public enum LEGEND_ORIENTATION
+        {
             HORIZONTAL,
             VERTICAL
         }
@@ -48,151 +52,176 @@ namespace AwesomeCharts {
         private List<LegendEntryView> legendEntryViews = new List<LegendEntryView>();
         private bool isDirty = false;
 
-        public int IconSize {
+        public int IconSize
+        {
             get { return iconSize; }
-            set {
+            set
+            {
                 iconSize = value;
                 SetDirty();
             }
         }
 
-        public int IconSpacing {
+        public int IconSpacing
+        {
             get { return iconSpacing; }
-            set {
+            set
+            {
                 iconSpacing = value;
                 SetDirty();
             }
         }
 
-        public int ItemWidth {
+        public int ItemWidth
+        {
             get { return itemWidth; }
-            set {
+            set
+            {
                 itemWidth = value;
                 SetDirty();
             }
         }
 
-        public int ItemHeight {
+        public int ItemHeight
+        {
             get { return itemHeight; }
-            set {
+            set
+            {
                 itemHeight = value;
                 SetDirty();
             }
         }
 
-        public int ItemSpacing {
+        public int ItemSpacing
+        {
             get { return itemSpacing; }
-            set {
+            set
+            {
                 itemSpacing = value;
                 SetDirty();
             }
         }
 
-        public Sprite IconImage {
+        public Sprite IconImage
+        {
             get { return iconImage; }
-            set {
+            set
+            {
                 iconImage = value;
                 SetDirty();
             }
         }
 
-        public int TextSize {
+        public int TextSize
+        {
             get { return textSize; }
-            set {
+            set
+            {
                 textSize = value;
                 SetDirty();
             }
         }
 
-        public Color TextColor {
+        public Color TextColor
+        {
             get { return textColor; }
-            set {
+            set
+            {
                 textColor = value;
                 SetDirty();
             }
         }
 
-        public LEGEND_ALIGNMENT Alignment {
+        public LEGEND_ALIGNMENT Alignment
+        {
             get { return alignment; }
-            set {
+            set
+            {
                 alignment = value;
                 SetDirty();
             }
         }
 
-        public LEGEND_ORIENTATION Orientation {
+        public LEGEND_ORIENTATION Orientation
+        {
             get { return orientation; }
-            set {
+            set
+            {
                 orientation = value;
                 SetDirty();
             }
         }
 
-        public List<LegendEntry> Entries {
+        public List<LegendEntry> Entries
+        {
             get { return entries; }
-            set {
+            set
+            {
                 entries = value;
                 SetDirty();
             }
         }
 
-        private void SetDirty() {
+        private void SetDirty()
+        {
             isDirty = true;
         }
 
-        void Awake() {
-            if (entries == null) {
+        void Awake()
+        {
+            if (entries == null)
+            {
                 entries = new List<LegendEntry>();
             }
-            if(viewCreator == null){
+            if (viewCreator == null)
+            {
                 viewCreator = new ViewCreator();
             }
         }
 
-        void Start() {
-            ClearEditModeObjects();
+        void Start()
+        {
             DrawLegendEntries();
         }
 
-        private void OnValidate() {
+        private void OnValidate()
+        {
             isDirty = true;
         }
 
-        protected override void Update() {
-            if (isDirty) {
+        protected override void Update()
+        {
+            if (isDirty)
+            {
                 DrawLegendEntries();
                 isDirty = false;
             }
         }
 
-        private void DrawLegendEntries() {
+        private void DrawLegendEntries()
+        {
             UpdateLegendEntryViewInstances(entries.Count);
-            for (int i = 0; i < entries.Count; i++) {
+            for (int i = 0; i < entries.Count; i++)
+            {
                 SetupLegendEntryView(legendEntryViews[i], entries[i], i);
             }
             HideAllChildrenInInspector();
         }
-
-        private void ClearEditModeObjects() {
-            int children = transform.childCount;
-            for (int i = 0; i < children; i++) {
-                DestroyImmediate(transform.GetChild(0).gameObject);
-            }
-            legendEntryViews.Clear();
-        }
-
-        private void UpdateLegendEntryViewInstances(int requiredCount) {
+        private void UpdateLegendEntryViewInstances(int requiredCount)
+        {
             int currentEntriesCount = legendEntryViews.Count;
 
             int missingEntiresCount = requiredCount - currentEntriesCount;
-            while (missingEntiresCount > 0) {
+            while (missingEntiresCount > 0)
+            {
                 LegendEntryView entryView = InstantiateLegendEntryView();
                 legendEntryViews.Add(entryView);
                 missingEntiresCount--;
             }
 
             int redundantEntriesCount = currentEntriesCount - requiredCount;
-            while (redundantEntriesCount > 0) {
+            while (redundantEntriesCount > 0)
+            {
                 LegendEntryView target = legendEntryViews[legendEntryViews.Count - 1];
                 DestroyDelayed(target.gameObject);
                 legendEntryViews.Remove(target);
@@ -200,17 +229,21 @@ namespace AwesomeCharts {
             }
         }
 
-        private LegendEntryView InstantiateLegendEntryView() {
+        private LegendEntryView InstantiateLegendEntryView()
+        {
             return viewCreator.InstantiateLegendEntry(entryViewPrefab, transform, PivotValue.BOTTOM_LEFT);
         }
 
-        private void HideAllChildrenInInspector() {
-            foreach (Transform child in transform) {
+        private void HideAllChildrenInInspector()
+        {
+            foreach (Transform child in transform)
+            {
                 child.hideFlags = HideFlags.HideInHierarchy | HideFlags.HideInInspector;
             }
         }
 
-        private void SetupLegendEntryView(LegendEntryView view, LegendEntry entry, int index) {
+        private void SetupLegendEntryView(LegendEntryView view, LegendEntry entry, int index)
+        {
             RectTransform viewTransform = view.GetComponent<RectTransform>();
             viewTransform.sizeDelta = new Vector2(ItemWidth, ItemHeight);
             viewTransform.pivot = PivotFromAlignment(alignment);
@@ -226,20 +259,26 @@ namespace AwesomeCharts {
             view.IconSpacing = IconSpacing;
         }
 
-        private Vector3 CalculateLegendEntryPosition(int index) {
-            if (orientation == LEGEND_ORIENTATION.VERTICAL) {
+        private Vector3 CalculateLegendEntryPosition(int index)
+        {
+            if (orientation == LEGEND_ORIENTATION.VERTICAL)
+            {
                 return new Vector3(0f,
                                    CalculateVerticalEntryPosition(alignment, index),
                                    0f);
-            } else {
+            }
+            else
+            {
                 return new Vector3(CalculateHorizontalEntryPosition(alignment, index),
                                    0f,
                                    0f);
             }
         }
 
-        private float CalculateVerticalEntryPosition(LEGEND_ALIGNMENT alignment, int index) {
-            switch (alignment) {
+        private float CalculateVerticalEntryPosition(LEGEND_ALIGNMENT alignment, int index)
+        {
+            switch (alignment)
+            {
                 case LEGEND_ALIGNMENT.BOTTOM_LEFT:
                 case LEGEND_ALIGNMENT.BOTTOM_RIGHT:
                     return index * (ItemHeight + ItemSpacing);
@@ -251,8 +290,10 @@ namespace AwesomeCharts {
             }
         }
 
-        private float CalculateHorizontalEntryPosition(LEGEND_ALIGNMENT alignment, int index) {
-            switch (alignment) {
+        private float CalculateHorizontalEntryPosition(LEGEND_ALIGNMENT alignment, int index)
+        {
+            switch (alignment)
+            {
                 case LEGEND_ALIGNMENT.BOTTOM_LEFT:
                 case LEGEND_ALIGNMENT.TOP_LEFT:
                     return index * (ItemWidth + ItemSpacing);
@@ -264,8 +305,10 @@ namespace AwesomeCharts {
             }
         }
 
-        private Vector2 PivotFromAlignment(LEGEND_ALIGNMENT alignment) {
-            switch (alignment) {
+        private Vector2 PivotFromAlignment(LEGEND_ALIGNMENT alignment)
+        {
+            switch (alignment)
+            {
                 case LEGEND_ALIGNMENT.TOP_LEFT:
                     return PivotValue.TOP_LEFT;
                 case LEGEND_ALIGNMENT.TOP_RIGHT:
@@ -279,8 +322,10 @@ namespace AwesomeCharts {
             }
         }
 
-        private Vector2 AnchorsFromAlignment(LEGEND_ALIGNMENT alignment) {
-            switch (alignment) {
+        private Vector2 AnchorsFromAlignment(LEGEND_ALIGNMENT alignment)
+        {
+            switch (alignment)
+            {
                 case LEGEND_ALIGNMENT.TOP_LEFT:
                     return new Vector2(0f, 1f);
                 case LEGEND_ALIGNMENT.TOP_RIGHT:
