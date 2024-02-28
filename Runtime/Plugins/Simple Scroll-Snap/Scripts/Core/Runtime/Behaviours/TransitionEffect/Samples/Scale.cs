a4c86d20cc7b20d7eceb7346ae367ef93fbb28b7
Jaime Ignacio Arancibia Soto <150153316+JaimeArancibiaDev@users.noreply.github.com>
2024-02-28 11:49:44 -0300

    Edited scale script

--- Runtime/Plugins/Simple Scroll-Snap/Scripts/Core/Runtime/Behaviours/TransitionEffect/Samples/Scale.cs
+++ Runtime/Plugins/Simple Scroll-Snap/Scripts/Core/Runtime/Behaviours/TransitionEffect/Samples/Scale.cs
@@ -7,8 +7,15 @@ namespace DanielLochner.Assets.SimpleScrollSnap
 {
     public class Scale : TransitionEffectBase<RectTransform>
     {
+        [SerializeField] private byte index = 0;
         public override void OnTransition(RectTransform rectTransform, float scale)
         {
+            while (index > 0)
+            {
+                if (rectTransform.childCount > 0)
+                    rectTransform = (RectTransform)rectTransform.GetChild(0);
+                index--;
+            }
             rectTransform.localScale = Vector3.one * scale;
         }
     }
